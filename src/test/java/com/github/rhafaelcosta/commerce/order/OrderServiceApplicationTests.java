e8445234b892bd386f92250218ab4790385a9f65
Rhafael Freitas da Costa <rhafael.costa@lactec.com.br>
2026-01-16 16:56:26 -0300

    Criação da estrutura do projeto order-service

---
+++ src/test/java/com/github/rhafaelcosta/commerce/order/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rhafaelcosta.commerce.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
